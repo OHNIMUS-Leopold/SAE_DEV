a901b3744ccd5f354a72f034d353f1750f3e6374
OHNIMUS-Leopold <95077164+OHNIMUS-Leopold@users.noreply.github.com>
2022-05-30 12:11:44 +0200

    debut_page_contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>
-    <div class="text-center">
-      Contact
+  <div class="grid grid-cols-2 my-24">
+    <div class="mt-[167px]">
+      <h1 class="text-orange font-Poppins text-center font-bold text-5xl leading-[3rem] tracking-normal uppercase">
+        Nous trouver...
+      </h1>
+      <div class="grid grid-cols-2 mt-[66px] mb-[34px]">
+        <div>
+          <h2 class="text-ocean-blue font-Poppins text-center font-bold text-[21px] leading-[21px] tracking-normal">
+            Notre adresse
+          </h2>
+          <p class="text-black text-center mt-[14px] font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+            424 Olympic-ro, Songpa-gu
+            <br/>
+            Parc Olympique de Séoul
+            <br/>
+            Corée du Sud
+          </p>
+        </div>
+        <div>
+          <h2 class="text-pink font-Poppins text-center font-bold text-[21px] leading-[21px] tracking-normal">
+            Notre téléphone
+          </h2>
+          <p class="text-black text-center mt-[14px] font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+            (+82) (8) 29 66 43 54
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2 class="text-deep-blue font-Poppins text-center font-bold text-[21px] leading-[21px] tracking-normal">
+          Notre e-mail
+        </h2>
+        <p class="text-black text-center mt-[14px] font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+            contact@pop-konnect.com
+          </p>
+      </div>
     </div>
-    <hr/>
+    <div class="mr-[92px]">
+      <img src="../assets/img/contact.png" alt="carte">
+    </div>
+  </div>
+  <div class="bg-gradient-to-r from-d1 via-d2 to-d3 rounded-[20px] mb-24">
+aaaaaaaaaaaaaaaa
   </div>
 </template>
 
