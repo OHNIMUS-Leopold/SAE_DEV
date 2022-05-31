22f42c6cd912be34557ea22c8bddca2f9fc0357e
OHNIMUS-Leopold <95077164+OHNIMUS-Leopold@users.noreply.github.com>
2022-05-31 17:14:22 +0200

    page_artistes_faite

--- src/views/PageArtisteView.vue
+++ src/views/PageArtisteView.vue
@@ -1,4 +1,146 @@
 <template>
-page artiste
+    <div class="mt-[72px]">
+        <h1 class="text-pink text-center font-Poppins font-bold text-5xl leading-[3rem] tracking-normal uppercase">
+            Qui sont blackpink ?
+        </h1>
+        <p class="text-black font-Poppins font-normal text-[15px] leading-[26px] tracking-normal py-14 mx-[92px]">
+            Blackpink (stylisé en majuscule ou BLΛƆKPIИK, hangeul : 블랙핑크 ; RR : Beullaegpingkeu), est un girl group sud-coréen formé en 2016 par YG Entertainment. C'est le deuxième groupe féminin à débuter, après 2NE1, au sein de la YG Entertainment. Le groupe est composé de quatre membres se prénommant Jennie, Lisa, Jisoo et Rosé. Elles forment un groupe multinational, venant de Corée du Sud, de Thaïlande, d'Australie et de Nouvelle-Zélande. Le groupe a fait ses débuts le 8 août 2016, avec leur premier album single, Square One, qui a donné naissance à Boombayah, leur première chanson numéro un en Corée du Sud, ainsi que Whistle, leur premier tube numéro un sur le classement du Billboard World Digital Songs. Avec le succès commercial précoce du groupe, elles ont été reconnues comme le nouvel artiste de l'année aux 31e Golden Disk Awards et aux 26e Seoul Music Awards.
+            <br /><br />
+            Blackpink est le groupe de K-pop féminin le plus haut-classé sur le Billboard Hot 100 et le Billboard 200, culminant respectivement à la treizième place (avec Ice Cream sortie en 2020) et à la deuxième place (avec The Album sortie en 2020). Elles sont également le premier et le seul groupe féminin de K-pop à entrer dans le classement du Billboard's Emerging Artists et à le culminer, et le premier groupe féminin de K-pop à avoir quatre singles numéro un sur le classement du Billboard's World Digital Song Sales. En 2018, au moment de sa sortie, Ddu-Du Ddu-Du, était le clip vidéo sud-coréen le plus visionné au cours des vingt-quatre premières heures sur YouTube, et en janvier 2019, il est devenu le clip vidéo le plus visionné pour un groupe de K-pop sur le site. Depuis septembre 2019, Blackpink est le groupe musical avec le plus d'abonnés sur YouTube.
+            <a href="https://fr.wikipedia.org/wiki/Blackpink" class="text-pink">Voir plus..</a>
+        </p>
+    </div>
+    <div class="grid grid-cols-2">
+        <div class="bg-black mt-44 ml-[92px] mb-auto px-11 py-20">
+            <h2 class="text-ocean-blue mb-5 font-Poppins font-bold text-[21px] leading-[21px] tracking-normal">Rosé</h2>
+            <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+                Rosé (로제, ro-je) est une chanteuse sud-coréenne, membre du groupe féminin BLACKPINK. C’est la chanteuse principale du groupe. 
+                <br />
+                Elle a débuté officiellement le 8 août 2016 avec Square One, puis en solo le 12 mars 2021 avec le single "R".
+                <a href="https://k-pop.fandom.com/fr/wiki/Ros%C3%A9" class="text-ocean-blue">Voir plus..</a>
+            </p>
+        </div>
+        <div>
+            <img src="../assets/img/blackpink/rose.jpg" alt="rosé">
+        </div>
+    </div>
+    <div class="grid grid-cols-2 mt-44">
+        <div>
+            <img src="../assets/img/blackpink/lisa.jpg" alt="lisa">
+        </div>
+        <div class="bg-black mt-44 mr-[92px] mb-auto px-11 py-20">
+            <h2 class="text-orange mb-5 font-Poppins font-bold text-[21px] leading-[21px] tracking-normal">Lisa</h2>
+            <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+                Lisa (리사, li-sa) est une chanteuse, danseuse et rappeuse thaïlandaise, membre du groupe féminin BLACKPINK. Elle a débuté officiellement le 8 août 2016 avec Square One, devenant la première artiste non-coréenne à débuter dans l'agence.
+                <a href="https://k-pop.fandom.com/fr/wiki/Lisa" class="text-orange">Voir plus..</a>
+            </p>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 mt-44">
+        <div class="bg-black mt-44 ml-[92px] mb-auto px-11 py-20">
+            <h2 class="text-deep-blue mb-5 font-Poppins font-bold text-[21px] leading-[21px] tracking-normal">Jisoo</h2>
+            <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+                Jisoo ou Ji Soo (지수) est une chanteuse sud-coréenne, membre du groupe féminin BLACKPINK. Elle a débuté officiellement le 8 août 2016 avec "Boombayah".
+                <a href="https://k-pop.fandom.com/fr/wiki/Jisoo_(BLACKPINK)" class="text-deep-blue">Voir plus..</a>
+            </p>
+        </div>
+        <div>
+            <img src="../assets/img/blackpink/jisoo.jpg" alt="jisoo">
+        </div>
+    </div>
+        <div class="grid grid-cols-2 mt-44">
+        <div>
+            <img src="../assets/img/blackpink/jennie.jpg" alt="jennie">
+        </div>
+        <div class="bg-black mt-44 mr-[92px] mb-auto px-11 py-20">
+            <h2 class="text-pink mb-5 font-Poppins font-bold text-[21px] leading-[21px] tracking-normal">Jennie</h2>
+            <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">
+                Jennie (제니, jeni) est une chanteuse et rappeuse sud-coréenne, membre du groupe féminin BLACKPINK. Après plusieurs collaborations avec des artistes YG Entertainment, elle a débuté officiellement le 8 août 2016 avec le groupe.
+                <a href="https://k-pop.fandom.com/fr/wiki/Jennie" class="text-pink">Voir plus..</a>
+            </p>
+        </div>
+    </div>
+    <div class="slider2 mt-[138px]">
+    <div class="slide-track2">
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/28d1131_CLzmMmdAmmVT9Lh_t6ukjHG7.jpg" alt="image 1">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/1306024.jpg" alt="image 2">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/blackpink2.jpg" alt="image 3">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/blackpink.jpg" alt="image 4">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/645531.jpg" alt="image 5">
+      </div>
+
+    <!--SLIDES DOUBLEES-->
+
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/28d1131_CLzmMmdAmmVT9Lh_t6ukjHG7.jpg" alt="image 1">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/1306024.jpg" alt="image 2">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/blackpink2.jpg" alt="image 3">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/blackpink.jpg" alt="image 4">
+      </div>
+      <div class="slide2">
+        <img src="../assets/img/blackpink/carousel/645531.jpg" alt="image 5">
+      </div>
+    </div>
+    </div>
+    <div class="bg-black mx-[650px] flex rounded-[10px] py-[14px] px-[30px] mt-11 mb-24 space-x-7">
+        <a href="https://www.instagram.com/blackpinkofficial/"><img src="../components/icons/png/instagram-blanc.png" alt="instagram"></a>
+        <a href="https://twitter.com/BLACKPINK"><img src="../components/icons/png/twitter-blanc.png" alt="twitter"></a>
+        <a href="https://www.youtube.com/channel/UCOmHUn--16B90oW2L6FRR3A"><img src="../components/icons/png/youtube-blanc.png" alt="youtube"></a>
+    </div>
 </template>
 
+<style>
+
+.slider2 {
+  height: 596px;
+  margin: auto;
+  position: relative;
+  width: 90%;
+  display: grid;
+  place-items: center;
+  overflow: hidden;
+}
+
+.slide-track2 {
+  display: flex;
+  width: calc(1086px * 10);
+  animation: scroll 40s linear infinite;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-1086px * 5));
+  }
+}
+
+.slide2 {
+  height: 596px;
+  width: 1086px;
+  display: flex;
+  align-items: center;
+  padding: 15px;
+}
+
+img {
+  width: 100%;
+}
+
+</style>
