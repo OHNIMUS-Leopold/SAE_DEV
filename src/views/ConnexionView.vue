79c84dc8bc3fdeade598b779819d60a6ce94d1af
OHNIMUS-Leopold <95077164+OHNIMUS-Leopold@users.noreply.github.com>
2022-06-01 15:24:47 +0200

    chemin_page_connexion

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -8,7 +8,7 @@
 
       <input style="filter:drop-shadow(0px 10px 10px gray)" class="outline-none focus:ring focus:ring-deep-blue mb-11 mx-[74px] h-14 rounded-[10px] placeholder:text-black pl-6" id="password" type="password" name="password" placeholder="Mot de passe">
 
-      <input type="submit" value="SE CONNECTER" style="filter:drop-shadow(0px 10px 10px gray)" class="text-white uppercase rounded-[10px] mb-[22px] font-Poppins font-bold text-[15px] leading-[26px] mx-[270px] py-3 tracking-normal bg-ocean-blue hover:text-black">
+      <RouterLink to="/profil" class="contents"><input type="submit" value="SE CONNECTER" style="filter:drop-shadow(0px 10px 10px gray)" class="text-white uppercase rounded-[10px] mb-[22px] font-Poppins font-bold text-[15px] leading-[26px] mx-[270px] py-3 tracking-normal bg-ocean-blue hover:text-black"></RouterLink>
     </form>
     <div class="flex justify-center pb-12">
       <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">Pas de compte ? Commandez votre billet et inscrivez-vous</p>
